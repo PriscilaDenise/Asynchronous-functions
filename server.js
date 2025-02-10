39267564c0237265506082a9c90da2b0a248342f
Denise Priscila <denisepriscilamuwanguzi@gmail.com>
2025-02-10 19:46:11 +0300

    server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require ('express');
 
 const application = express();
 
-const port = 3000;
+const port = 2000;
 
 application.listen(port, () => {
-    console.log("Server is running through port 3000")
+    console.log("Server is running through port 2000")
 })
